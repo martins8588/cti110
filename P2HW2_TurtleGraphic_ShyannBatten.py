a2dc45200c2db3796ffe2f74b7ffb1d14902871a
martins8588 <55702441+martins8588@users.noreply.github.com>
2019-10-02 10:09:52 -0400

    Add files via upload

---
+++ P2HW2_TurtleGraphic_ShyannBatten.py
@@ -0,0 +1,29 @@
+# Turtle Graphics
+    # 09/23/19
+    # CTI-110 P2HW2 - Turtle Graphic
+    # Shyann Batten
+
+import turtle
+tim = turtle.Turtle()
+turtle.circle(50)
+turtle.penup()
+turtle.setposition(-120, 0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(60,60)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(-60, 60)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(-180, 60)
+turtle.pendown()
+turtle.circle(50)
+
+    
